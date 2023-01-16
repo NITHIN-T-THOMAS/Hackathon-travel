15bb875647872091dc487eb71559fa54eddb383b
karthik.sk <karthik.sk@cabotsolutions.com>
2023-01-16 18:11:33 +0530

    multi-threading added

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const cors = require("cors");
+const cluster = require('cluster');
+const os = require('os');
+require('dotenv').config()
+
 const app = express();
 
-require('dotenv').config()
+const numCpu = (os.cpus.length == 0)?4:os.cpus.length;
+
 const port = process.env.PORT || 8005;
 app.set('port', port);
 app.use(cors())
@@ -18,9 +23,23 @@ sequelize
     //configurePassport();
     // new CreateModels();
     // new setRoutes(app);
-    app.listen(app.get('port'),app.get('url'), () => {
-      console.log(`\n Listening on port   ${app.get('url')}:${app.get('port')}  \n`);
-    });
+    if(cluster.isMaster){
+      console.log(numCpu);
+      for (let i = 0; i < numCpu; i++) {
+        cluster.fork();
+      }
+
+      cluster.on("exit", (worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} died`);
+        console.log("Let's fork another worker!");
+        cluster.fork(); //use cluster.worker.kill() - kill fork after result send
+      });
+    }
+    else{
+      app.listen(app.get('port'),app.get('url'), () => {
+        console.log(`\n Listening on port   ${app.get('url')}:${app.get('port')}@${process.pid}  \n`);
+      });
+    }
   })
   .catch((err) => {
     console.log('Unable to connect to the database:', err);
